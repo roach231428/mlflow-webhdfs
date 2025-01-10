b888b8f0e2afc978752376a0d4a6c57acd4672a2
roach231428 <roach231428@users.noreply.github.com>
2025-01-10 16:30:04 +0800

    Update some info in setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ setup(
     version="0.0.1",
     description="MLflow WebHDFS Plugins",
     packages=find_packages(),
+    author="roach231428",
+    author_email="roach231428@gmail.com",
+    url="https://github.com/roach231428/mlflow-webhdfs",
     install_requires=[
         "mlflow",
         "hdfs",
@@ -13,5 +16,6 @@ setup(
         "mlflow.artifact_repository": [
             "webhdfs=store.artifact.webhdfs_artifact_repo:WebHdfsArtifactRepository",
         ],
-    }
+    },
+    license="Apache License 2.0",
 )
